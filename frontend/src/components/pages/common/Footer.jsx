493efef2a4a4c07b2ea4039475ceee7b228851c0
KevinHatchyard <kevin@hatchyard.io>
2022-09-06 19:04:10 +0530

    changes to the footer

--- frontend/src/components/pages/common/Footer.jsx
+++ frontend/src/components/pages/common/Footer.jsx
@@ -3,28 +3,48 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
+import NameLogo from "../../../assets/logos/NameLogo.svg";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
+      {"Copyright © 2022 All rights reserved | "}
+      <Link color="inherit" href="#">
         PrimePsyche
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
+      </Link>
     </Typography>
   );
 }
 
 export default function Footer() {
+  const pages = [
+    {
+      label: "Home",
+      path: "/primepsyche/",
+    },
+    {
+      label: "About",
+      path: "#",
+    },
+    {
+      label: "Our Team",
+      path: "#",
+    },
+    {
+      label: "Help",
+      path: "#",
+    },
+  ];
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "100vh",
       }}
     >
       <CssBaseline />
@@ -36,14 +56,53 @@ export default function Footer() {
           px: 2,
           mt: "auto",
           backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
+            theme.palette.prime_psycheColors.prime_psyche_darker_green2,
         }}
       >
-        <Container maxWidth="sm">
-          <Typography variant="body1"></Typography>
-          <Copyright />
+        <Container>
+          <Box
+            sx={{
+              justifyContent: "center",
+              display: "flex",
+            }}
+          >
+            <img alt="logo" src={NameLogo} />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "center",
+            }}
+          >
+            {pages.map((page) => (
+              <Link style={{ textDecoration: "none" }} to={page.path}>
+                <Button key={page.label} sx={{ my: 2, display: "block" }}>
+                  {page.label}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              justifyContent: "center",
+              display: "flex",
+              pb: 2,
+            }}
+          >
+            <InstagramIcon sx={{ mx: 2 }} />
+            <TwitterIcon sx={{ mx: 2 }} />
+            <FacebookIcon sx={{ mx: 2 }} />
+          </Box>
+          <Box
+            sx={{
+              justifyContent: "center",
+              display: "flex",
+            }}
+          >
+            <Typography variant="body1"></Typography>
+            <Copyright />
+          </Box>
         </Container>
       </Box>
     </Box>
