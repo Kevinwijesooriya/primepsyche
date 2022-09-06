8ee35f10e3700faf51b3d00bede0d94e042f7cb1
KevinHatchyard <kevin@hatchyard.io>
2022-09-06 19:05:26 +0530

    added forum routes

--- frontend/src/components/pages/forum/routes/index.js
+++ frontend/src/components/pages/forum/routes/index.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import ForumHomePage from "../views";
+import AddPost from "../views/AddPost";
+import IconBreadcrumbs from "../views/components/BreadCrums";
+import EditPost from "../views/EditPost";
 
 const ForumRoutes = [
   {
     path: "forum",
-    element: <ForumHomePage />,
-    //HINT
-    // children: [
-    //   { path: "", element: <Navigate to="view" /> },
-    //   { path: "view", element: <ViewComponent /> },
-    //   { path: "add", element: <AddComponent /> },
-    // ],
+    element: <IconBreadcrumbs />,
+    children: [
+      { path: "", element: <Navigate to="view" /> },
+      { path: "view", element: <ForumHomePage /> },
+      { path: "add", element: <AddPost /> },
+      { path: "edit", element: <EditPost /> },
+    ],
   },
+  // { path: "forum", element: <ForumHomePage /> },
+  // { path: "forum/add", element: <AddPost /> },
 ];
 
 export default ForumRoutes;
