98e939581447c749185e71635c0ab83cbbc33790
Sithara-tech <sitharakumarasingha@gmail.com>
2022-09-08 22:19:03 +0530

    Date picker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import { PrimePsycheTheme } from "./core/styles/theme/PrimePsycheTheme";
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={PrimePsycheTheme}>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
+    </LocalizationProvider>
   </ThemeProvider>
 );
