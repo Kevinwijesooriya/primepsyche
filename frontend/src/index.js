34a4666b266d1af5e393e1a31e917e483bdd1dda
Kevin Wijesooriya <106952701+KevinHatchyard@users.noreply.github.com>
2022-09-08 21:42:55 +0530

    auth provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import { PrimePsycheTheme } from "./core/styles/theme/PrimePsycheTheme";
+import { AuthProvider } from "./core/context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={PrimePsycheTheme}>
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </React.StrictMode>
   </ThemeProvider>
 );
