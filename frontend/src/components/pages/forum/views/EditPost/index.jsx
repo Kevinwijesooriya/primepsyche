f5c030eb13e33b54342c652474d2e231a9787f8b
Kevin Wijesooriya <106952701+KevinHatchyard@users.noreply.github.com>
2022-09-08 21:47:35 +0530

    integration

--- frontend/src/components/pages/forum/views/EditPost/index.jsx
+++ frontend/src/components/pages/forum/views/EditPost/index.jsx
@@ -7,13 +7,31 @@ import Container from "@mui/material/Container";
 import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
 import InputLabel from "@mui/material/InputLabel";
 import Box from "@mui/material/Box";
-import { ImageUploadButton, WarningButton } from "../../styles";
+import { ImageUploadButton, StyledLink, WarningButton } from "../../styles";
 import { Stack } from "@mui/material";
+import ForumPostAPI from "../../../../../core/services/ForumPostAPI";
+import { useParams } from "react-router-dom";
 
 const EditPost = () => {
   const [files, setFiles] = React.useState();
+  const [postsData, setPostsData] = React.useState({
+    title: "",
+    description: "",
+    image: "",
+  });
+  const { title, description, image } = postsData;
+  const params = useParams();
+  const postId = params.id;
+  async function fetchData() {
+    const response = await ForumPostAPI.getOne(postId);
+    setPostsData(response.data.data);
+  }
+  React.useEffect(() => {
+    fetchData();
+  }, []);
   function handleChange(e) {
     console.log(e.target.files);
+    console.log("postsData", postsData);
     setFiles(URL.createObjectURL(e.target.files[0]));
   }
   return (
@@ -34,7 +52,14 @@ const EditPost = () => {
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
             <InputLabel>Title</InputLabel>
-            <TextField required id="title" name="title" fullWidth multiline />
+            <TextField
+              required
+              id="title"
+              name="title"
+              fullWidth
+              multiline
+              defaultValue={title || ""}
+            />
           </Grid>
           <Grid item xs={12} sm={6}></Grid>
           <Grid item xs={12} sm={6}>
@@ -45,20 +70,29 @@ const EditPost = () => {
               name="description"
               fullWidth
               multiline
+              defaultValue={description || ""}
             />
           </Grid>
           <Grid item xs={12}>
             <InputLabel>Enhance your post with an image!</InputLabel>
             <ImageUploadButton component="label">
               <input type="file" hidden onChange={handleChange} />
-              {files ? (
+              {files && (
                 <img
                   alt="forum_post"
                   src={files}
                   style={{ minHeight: 600, minWidth: 600 }}
                 />
-              ) : (
+              )}{" "}
+              {!image && (
                 <ImageOutlinedIcon sx={{ minHeight: 600, minWidth: 600 }} />
+              )}
+              {image && (
+                <img
+                  alt="forum_post"
+                  src={image}
+                  style={{ minHeight: 600, minWidth: 600 }}
+                />
               )}
             </ImageUploadButton>
           </Grid>
@@ -69,7 +103,9 @@ const EditPost = () => {
               sx={{ display: "flex", justifyContent: "flex-end" }}
             >
               <Button>save</Button>
-              <WarningButton>Cancel</WarningButton>
+              <StyledLink to={"/primepsyche/forum/"}>
+                <WarningButton>Cancel</WarningButton>
+              </StyledLink>
             </Stack>
           </Grid>
         </Grid>
