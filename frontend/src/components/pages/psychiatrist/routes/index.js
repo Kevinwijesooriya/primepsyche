60671a32e5993a4ec829bc3c30345c7beb74d0d1
aroshana <rasitharoshana0215@gmail.com>
2022-09-08 13:20:02 +0530

    update routes in psychaitrist

--- frontend/src/components/pages/psychiatrist/routes/index.js
+++ frontend/src/components/pages/psychiatrist/routes/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import HelpHomePage from "../views";
+import view from "../views";
+import PsychaiatristBreadcrumbs from "../views/components/Breadcrumbs";
 
 const PsychiatristRoutes = [
   {
     path: "help",
-    element: <HelpHomePage />,
-    //HINT
-    // children: [
-    //   { path: "", element: <Navigate to="view" /> },
-    //   { path: "view", element: <ViewComponent /> },
-    //   { path: "add", element: <AddComponent /> },
-    // ],
+    // element: <HelpHomePage />,
+    element: <PsychaiatristBreadcrumbs />,
+
+    children: [
+      { path: "", element: <Navigate to="view" /> },
+      { path: "view", element: <HelpHomePage /> },
+      // { path: "view", element: <ViewComponent /> },
+      // { path: "add", element: <AddComponent /> },
+    ],
   },
 ];
 
