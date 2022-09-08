bc9ee175e9f72deded533facc102c1a3f31308fe
Kevin Wijesooriya <106952701+KevinHatchyard@users.noreply.github.com>
2022-09-08 21:39:16 +0530

    back end initialization

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import {} from "dotenv/config";
+import express, { json } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+const { connect } = mongoose;
+
+// Connect MongoDB.
+const URI = process.env.MONGODB_URL;
+connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection: " + err);
+    }
+  }
+);
+
+const app = express();
+app.use(json());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  `Server running on port ${port} 🔥`;
+  console.log(`Server running on port ${port} 🔥`);
+});
+
+//route imports
+import ForumPostRoute from "./routes/forumPost.js";
+
+//routes
+app.use(ForumPostRoute);
