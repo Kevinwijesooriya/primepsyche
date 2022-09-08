5e70ec57b6ebd559328768b4b69e4b1ff68b9172
Sithara-tech <sitharakumarasingha@gmail.com>
2022-09-08 22:22:08 +0530

    Event Breadcrumbs and pagination

---
+++ frontend/src/components/pages/event/views/components/Pagination.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+export default function BasicPagination(props) {
+    const { count } = props;
+    return (
+        <Stack spacing={2}>
+            <Pagination count={count} />
+            {/* <Pagination count={10} disabled /> */}
+        </Stack>
+    );
+}
