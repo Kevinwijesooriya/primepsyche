adbf2683b785df925907999e21e44e773089b357
Sithara-tech <sitharakumarasingha@gmail.com>
2022-09-08 22:19:52 +0530

    route implementation of event

--- frontend/src/components/pages/event/routes/index.js
+++ frontend/src/components/pages/event/routes/index.js
@@ -1,16 +1,23 @@
 import React from "react";
-import EventHomePage from "../views";
+import { Navigate } from "react-router-dom";
+import ViewComponent from "../views";
+// import AddComponent from "../views"
+import EditEvent from "../views/EditEvent";
+import AddEvent from "../views/AddEvent";
+import EventBreadcrumbs from "../views/components/Breadcrumbs";
+import CusViewEvents from "../views/CusViewEvent";
 
 const EventRoutes = [
   {
     path: "events",
-    element: <EventHomePage />,
-    //HINT
-    // children: [
-    //   { path: "", element: <Navigate to="view" /> },
-    //   { path: "view", element: <ViewComponent /> },
-    //   { path: "add", element: <AddComponent /> },
-    // ],
+    element: <EventBreadcrumbs />,
+    children: [
+      { path: "", element: <Navigate to="view" /> },
+      { path: "view", element: <ViewComponent /> },
+      { path: "add", element: <AddEvent /> },
+      { path: "edit", element: <EditEvent /> },
+      { path: "CusView", element: <CusViewEvents /> },
+    ],
   },
 ];
 
