d19393932043f2d6faa522b87dbc7e82c3ef2f31
Kevin Wijesooriya <106952701+KevinHatchyard@users.noreply.github.com>
2022-09-09 17:52:49 +0530

    expandable view

--- frontend/src/components/pages/materials/views/ViewMusic/index.jsx
+++ frontend/src/components/pages/materials/views/ViewMusic/index.jsx
@@ -5,7 +5,7 @@ import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { Box, Button, Divider } from "@mui/material";
+import { Box, Button, Divider, ListItem } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import { PostContainer, StyledLink, WarningButtonOutlined } from "../../styles";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -16,15 +16,24 @@ import Stack from "@mui/material/Stack";
 import { Colors } from "../../../../../core/styles/theme/PrimePsycheTheme";
 import BasicPagination from "../components/Pagination";
 import AlertDialog from "../../../forum/views/DeleteConfirmation";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const ViewMusic = () => {
   const [open, setOpen] = React.useState(false);
+  const [expand, setExpand] = React.useState(false);
+  const [expandItem, setExpandItem] = React.useState(false);
 
   const onClickDelete = () => {
     setOpen(true);
   };
+  const onClickExpand = (id) => {
+    setExpandItem(id);
+    setExpand(!expand);
+  };
   const MusicList = [
     {
+      _id: "uef-1234",
       title: "Deep in the Ocean",
       artist: "Clair de Lune",
       genre: "Modern Classical",
@@ -33,6 +42,7 @@ const ViewMusic = () => {
         "https://pixabay.com/illustrations/eye-waterfall-city-the-shade-moon-462267/",
     },
     {
+      _id: "uef-12345s",
       title: "Deep in the Ocean",
       artist: "Clair de Lune",
       genre: "Modern Classical",
@@ -64,66 +74,99 @@ const ViewMusic = () => {
       {MusicList &&
         MusicList.map((music) => (
           <PostContainer item xs={12} md={6}>
-            <CardActionArea component="a" href="#">
-              <Card sx={{ display: "flex" }}>
-                <CardContent sx={{ flex: 1, p: 2 }}>
+            <ListItem sx={{ justifyContent: "space-between" }}>
+              <Stack direction="row">
+                <IconButton>
+                  <PlayArrowIcon />
+                </IconButton>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 90, display: { xs: "none", sm: "block" } }}
+                  alt="image"
+                  image={music.image}
+                />
+
+                <Box>
                   <Typography component="h2" variant="h5">
                     {music.title}
                   </Typography>
                   <Typography variant="subtitle1" color="text.secondary">
                     {music.artist}
                   </Typography>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    {music.genre}
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    {music.album}
-                  </Typography>
-                </CardContent>
-                <CardMedia
-                  component="img"
-                  sx={{ width: 160, display: { xs: "none", sm: "block" } }}
-                  alt="image"
-                  image={music.image}
-                />
-              </Card>
-              <Divider></Divider>
-              <Grid
-                container
-                spacing={3}
-                sx={{ backgroundColor: `${Colors.prime_psyche_gradient}` }}
-              >
-                <Grid item xs={12} sm={6}>
-                  <Stack direction="row" spacing={1}>
-                    <IconButton>
-                      <FavoriteIcon />
-                    </IconButton>
-                    <IconButton>
-                      <CommentIcon />
-                    </IconButton>
-                  </Stack>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Stack
-                    direction="row"
-                    spacing={1}
-                    sx={{ display: "flex", justifyContent: "flex-end" }}
-                  >
-                    <StyledLink to="/primepsyche/materials/editMusic">
-                      <Button variant="outlined" startIcon={<EditIcon />}>
-                        Edit
-                      </Button>
-                    </StyledLink>
-                    <WarningButtonOutlined
-                      startIcon={<DeleteIcon />}
-                      onClick={onClickDelete}
+                </Box>
+              </Stack>
+              <Box>
+                <Button>Download</Button>
+
+                <IconButton onClick={() => onClickExpand(music._id)}>
+                  <KeyboardArrowDownIcon />
+                </IconButton>
+              </Box>
+            </ListItem>
+            {expand && expandItem === music._id && (
+              <>
+                <CardActionArea component="a" href="#">
+                  <Card sx={{ display: "flex" }}>
+                    <CardContent sx={{ flex: 1, p: 2 }}>
+                      <Typography component="h2" variant="h5">
+                        {music.title}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary">
+                        {music.artist}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary">
+                        {music.genre}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary">
+                        {music.album}
+                      </Typography>
+                    </CardContent>
+                    <CardMedia
+                      component="img"
+                      sx={{ width: 160, display: { xs: "none", sm: "block" } }}
+                      alt="image"
+                      image={music.image}
+                    />
+                  </Card>
+                </CardActionArea>
+                <Divider></Divider>
+                <Grid
+                  container
+                  spacing={3}
+                  sx={{ backgroundColor: `${Colors.prime_psyche_gradient}` }}
+                >
+                  <Grid item xs={12} sm={6}>
+                    <Stack direction="row" spacing={1}>
+                      <IconButton>
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton>
+                        <CommentIcon />
+                      </IconButton>
+                    </Stack>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Stack
+                      direction="row"
+                      spacing={1}
+                      sx={{ display: "flex", justifyContent: "flex-end" }}
                     >
-                      Delete
-                    </WarningButtonOutlined>
-                  </Stack>
+                      <StyledLink to="/primepsyche/materials/editMusic">
+                        <Button variant="outlined" startIcon={<EditIcon />}>
+                          Edit
+                        </Button>
+                      </StyledLink>
+                      <WarningButtonOutlined
+                        startIcon={<DeleteIcon />}
+                        onClick={onClickDelete}
+                      >
+                        Delete
+                      </WarningButtonOutlined>
+                    </Stack>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </CardActionArea>
+              </>
+            )}
           </PostContainer>
         ))}
       <Box sx={{ display: "flex", justifyContent: "center", py: 2 }}>
